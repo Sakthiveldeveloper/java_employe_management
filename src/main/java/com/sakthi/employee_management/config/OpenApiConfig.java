77ac34c96cd5cc30fcb4e68083a435e303eaca61
Sakthivel A <sakthivel24498@gmail.com>
2026-07-11 03:43:42 +0530

    swagge dependencies added and roadmaps added briefly

---
+++ src/main/java/com/sakthi/employee_management/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.sakthi.employee.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI employeeManagementAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Employee Management System API")
+                        .description("Spring Boot Employee Management REST API")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Sakthivel A")
+                                .email("your-email@example.com")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation"));
+    }
+}
